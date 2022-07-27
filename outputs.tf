6678ba61755215d9f22167a7b6c9b8cbd539acea
kashiwaguma-hiro <kashiwaguma-hiro@dmm.com>
2022-07-27 12:20:16 +0900

    Added output variables

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,16 @@
+output "name" {
+  value = values(aws_iam_user.default.name)
+}
+
+output "arns" {
+  value = values(aws_iam_user.default.arn)
+}
+
 output "encrypted_passwords" {
+  value = values(aws_iam_user_login_profile.default.encrypted_password)
+}
+
+output "encrypted_passwords_map" {
   value = zipmap(values(aws_iam_user_login_profile.default)[*].user, values(aws_iam_user_login_profile.default)[*].encrypted_password)
 }
+
