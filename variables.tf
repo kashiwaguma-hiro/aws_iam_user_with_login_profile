936956d87bd70470b5c310bce1a3e6076aeb8125
kashiwaguma-hiro <kashiwaguma-hiro@dmm.com>
2022-07-28 09:37:19 +0900

    fix the variable name

--- variables.tf
+++ variables.tf
@@ -7,19 +7,19 @@ variable "iam_users" {
   description = "作成するIAM User情報"
 }
 
-variable "iam_user_force_destroy" {
+variable "force_destroy" {
   type        = bool
   description = "Destroy実行時に、Terraform管理外のMFA・キーを生成している場合でもユーザを削除する"
   default     = false
 }
 
-variable "login_password_length" {
+variable "password_length" {
   type        = number
   description = "生成する初期ログインパスワードの長さ"
   default     = 20
 }
 
-variable "login_password_reset_required" {
+variable "password_reset_required" {
   type        = bool
   description = "ログイン後のパスワードリセットを強制する"
   default     = true
